7dbb42867dd78ce0812668395a42fa65fabfaa29
Darko <dbozhinovski@gmail.com>
2022-06-09 08:27:30 +0200

    Civilizes the whole thing a bit; Adds placeholder stuff where needed

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,23 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 
 import { Clock } from './Clock';
-import Gallery from './Gallery';
+import { Gallery } from './Gallery';
+import { Counter } from './Counter';
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        {/* <img src={logo} class={styles.logo} alt="logo" />
+        <img src={logo} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a class={styles.link} href="https://github.com/solidjs/solid" target="_blank" rel="noopener noreferrer">
           Learn Solid
-        </a> */}
+        </a>
         {/* <Clock /> */}
-        <Gallery />
+        {/* <Counter /> */}
+        {/* <Gallery /> */}
       </header>
     </div>
   );
--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -10,12 +10,14 @@ interface FakerData {
 
 const CATEGORIES = ['any', 'animals', 'architecture', 'nature', 'people', 'tech', 'kittens', 'pokemon'];
 
-export default function Schedule() {
+export const Gallery = () => {
   const [category, setCategory] = createSignal('animals');
   const [quantity, setQuantity] = createSignal(10);
 
   const [gallery, { refetch }] = createResource(async () => {
-    const res = await fetch(`https://fakerapi.it/api/v1/images?_quantity=${quantity()}&_type=${category()}&_height=300`);
+    const res = await fetch(
+      `https://fakerapi.it/api/v1/images?_quantity=${quantity()}&_type=${category()}&_height=300`,
+    );
     const json = await res.json();
 
     return json.data;
@@ -23,34 +25,47 @@ export default function Schedule() {
 
   return (
     <>
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        refetch();
-      }}>
-        <input type="number" value={quantity()} onInput={(event) => { setQuantity(parseInt(event.currentTarget.value, 10)); refetch(); }} />
-        <select value={category()} onInput={(event) => { setCategory(event.currentTarget.value); refetch(); }}>
-          <For each={CATEGORIES}>
-            {(cat) => <option value={cat}>{cat}</option>}
-          </For>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          refetch();
+        }}
+      >
+        <input
+          type="number"
+          value={quantity()}
+          onInput={(event) => {
+            setQuantity(parseInt(event.currentTarget.value, 10));
+            refetch();
+          }}
+        />
+        <select
+          value={category()}
+          onInput={(event) => {
+            setCategory(event.currentTarget.value);
+            refetch();
+          }}
+        >
+          <For each={CATEGORIES}>{(cat) => <option value={cat}>{cat}</option>}</For>
         </select>
         <button>Refresh</button>
       </form>
       <div class={styles.gallery}>
         <For each={gallery()}>
-            {(item: FakerData) => (
-              <Suspense fallback={<p>loading...</p>}>
-                <Show when={item}>
-                  <section >
-                    <figure>
-                      <img src={item.url} />
-                      <figcaption>{item.title}</figcaption>
-                    </figure>
-                  </section>
-                </Show>
-              </Suspense>
-            )}
+          {(item: FakerData) => (
+            <Suspense fallback={<p>loading...</p>}>
+              <Show when={item}>
+                <section>
+                  <figure>
+                    <img src={item.url} />
+                    <figcaption>{item.title}</figcaption>
+                  </figure>
+                </section>
+              </Show>
+            </Suspense>
+          )}
         </For>
       </div>
     </>
   );
-}
+};
